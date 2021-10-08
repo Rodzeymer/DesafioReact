84bd9af4596ea62bc603275a8f50af19c51f0c2a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-08 14:50:30 -0300

    git de teste

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Component} from 'react';
+import './App.css';
+
+class App extends Component{
+    state = { 
+        posts:[]
+    };
+
+    componentDidMpunt(){
+
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <iframe class="video"
+   src="https://www.youtube.com/embed/ntT-N5f-Aks" allowfullscreen=""></iframe>
+            </div>
+        )
+    }
