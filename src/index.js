c893b0d44a726676456fee07d9bd0c2960b2eedf
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-08 14:31:09 -0300

    testando

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
     <div class="video">
     <iframe class="embed-responsive-item" 
     src="https://www.youtube.com/embed/ntT-N5f-Aks" allowfullscreen=""></iframe>
-      </div>
+    </div>
 
   </React.StrictMode>,
   document.getElementById('root'),
